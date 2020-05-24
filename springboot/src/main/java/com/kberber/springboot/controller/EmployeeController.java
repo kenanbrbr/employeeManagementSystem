aa7d583e3bbe444525168e5a26d00afd1a5fde92
kberber <kenan.berber@32bit.com.tr>
2020-05-24 20:59:51 +0300

    sorting changes

--- springboot/src/main/java/com/kberber/springboot/controller/EmployeeController.java
+++ springboot/src/main/java/com/kberber/springboot/controller/EmployeeController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.kberber.springboot.model.Employee;
 import com.kberber.springboot.service.EmployeeService;
@@ -24,7 +25,7 @@ public class EmployeeController {
 	
 	@GetMapping("/")
 	public String viewHomePage(Model model) {
-		return findPaginated(1, model);
+		return findPaginated(1, "firstName","asc", model);
 	}
 	
 	@GetMapping("/showNewEmployeeForm")
@@ -59,15 +60,24 @@ public class EmployeeController {
 	}
 	
 	@GetMapping("/page/{pageNo}")
-	public String findPaginated(@PathVariable (value = "pageNo") int pageNo, Model model) {
+	public String findPaginated(@PathVariable (value = "pageNo") int pageNo,
+			@RequestParam("sortField") String sortField,
+			@RequestParam("sortDir") String sortDir,
+			Model model) {
 		int pageSize = 5;
 		
-		Page<Employee> page = employeeService.findPaginated(pageNo, pageSize);
+		Page<Employee> page = employeeService.findPaginated(pageNo, pageSize,sortField,sortDir);
 		List<Employee> listEmployees = page.getContent();
 		
 		model.addAttribute("currentPage", pageNo);
 		model.addAttribute("totalPages", page.getTotalPages());
 		model.addAttribute("totalItems", page.getTotalElements());
+		
+		model.addAttribute("sortField", sortField);
+		model.addAttribute("sortDir", sortDir);
+		model.addAttribute("sortDir", sortDir);
+		model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
+		
 		model.addAttribute("listEmployees", listEmployees);
 		
 		return "index";
--- springboot/src/main/java/com/kberber/springboot/service/EmployeeServiceImpl.java
+++ springboot/src/main/java/com/kberber/springboot/service/EmployeeServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.kberber.springboot.model.Employee;
@@ -49,9 +50,11 @@ public class EmployeeServiceImpl implements EmployeeService {
 	}
 
 	@Override
-	public Page<Employee> findPaginated(int pageNo, int pageSize) {
-		// 
-		Pageable pageable = PageRequest.of(pageNo-1, pageSize);
+	public Page<Employee> findPaginated(int pageNo, int pageSize,String sortField, String sortDirection) {
+		//
+		Sort sort = sortDirection.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortField).ascending() :
+			Sort.by(sortField).descending();
+		Pageable pageable = PageRequest.of(pageNo-1, pageSize,sort);
 		return this.employeeRepository.findAll(pageable);
 	}
 
